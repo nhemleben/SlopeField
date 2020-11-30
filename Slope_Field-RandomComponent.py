4f5e66fec9736208e927c6c2351c05579f8ae8fb
nhemleben <nhemleben@gmail.com>
2020-11-29 22:35:12 -0500

    Update vector drawing

--- Slope_Field-RandomComponent.py
+++ Slope_Field-RandomComponent.py
@@ -1,58 +1,55 @@
-
 #slope field mark 3 with
 #linear interpolation of the acceleartion vector
 #between gridpoints
 #and amount of random movmeent from each piece
+#draws in vector lines
 import matplotlib.pyplot as plt
 import turtle
 import random
 import numpy as np
 
-
 def draw_vectors():
     drawer = turtle.Turtle()
+    drawer.shape("classic") #hould look like a vector
+    drawer.hideturtle() 
     for i in range(2*N):
         for j in range(2*N):
             x_val = ( i - N +1 ) * Spacer
-            y_val = ( i - N +1 ) * Spacer
-
-            flake.penup()   
-            flake.goto( x_val, y_val)                      
-            flake.pendown()   
-            flake.goto(x_val + vectors[i][j][0], y_val + vectors[i][j][1] )
-
+            y_val = ( j - N +1 ) * Spacer
+            drawer.penup()   
+            drawer.goto( x_val, y_val)                      
+            drawer.pendown()   
+            drawer.goto(x_val + vectors[i][j][0], y_val + vectors[i][j][1] )
 
 def next_step(x,y):
     x_flr = ( np.floor( (x) /Spacer) -1 ) + N
     y_flr = ( np.floor( (y) /Spacer) -1 ) + N
-
-#the edge cases shouldn't be reachable
+#the edge cases shouldn't be reachable so no if statment
     x_ind = round(x_flr)
     y_ind = round(y_flr)
 
     a_1 = x/Spacer - np.floor(x/Spacer)
-    a_2 = 1-a_1
     b_1 = y/Spacer -np.floor(y/Spacer)
-    b_2 = 1-b_1
+    a_2, b_2 = 1-a_1, 1-b_1
 
     f_1 = S * vectors[x_ind][y_ind][0]
     f_2 = S * vectors[x_ind+1][y_ind][0]
     f_3 = S * vectors[x_ind][y_ind+1][0]
     f_4 = S * vectors[x_ind+1][y_ind+1][0]
-
     dx = f_1*a_2*b_2 + f_2*a_1*b_2 + f_3*a_2*b_1 + f_4*a_1*b_1 
 
-    f_1 = S * vectors[x_ind][y_ind][1]
-    f_2 = S * vectors[x_ind+1][y_ind][1]
-    f_3 = S * vectors[x_ind][y_ind+1][1]
-    f_4 = S * vectors[x_ind+1][y_ind+1][1]
+    g_1 = S * vectors[x_ind][y_ind][1]
+    g_2 = S * vectors[x_ind+1][y_ind][1]
+    g_3 = S * vectors[x_ind][y_ind+1][1]
+    g_4 = S * vectors[x_ind+1][y_ind+1][1]
+    dy = g_1*a_2*b_2+ g_2*a_1*b_2 + g_3*a_2*b_1 + g_4*a_1*b_1 
 
-    dy = f_1*a_2*b_2+ f_2*a_1*b_2 + f_3*a_2*b_1 + f_4*a_1*b_1 
-        
-    #random Component now
-    dx_random = np.random.normal(0, S_random)
-    dy_random = np.random.normal(0, S_random)
-
+    if(Random_Stuff): #random Component now
+        dx_random = np.random.normal(0, S_random)
+        dy_random = np.random.normal(0, S_random)
+    else:
+        dx_random = 0
+        dy_random = 0
     #return deterministic plus random component
     return [ dx + dx_random, dy + dy_random ]
 
@@ -63,10 +60,11 @@ wn.tracer(0)
 
 flakes=[]
 Colors=["grey"]
-num_flakes = 1
+num_flakes = 100
 vectors= [ ]    #set up array of vectors to use as slope field
-N = 35 # number of points in grid
-Spacer = 10     #number of pixels between gridpoints of vectorfield
+N = 15 # number of points in grid
+Spacer = 20     #number of pixels between gridpoints of vectorfield
+Random_Stuff = True
 
 for i in range(2*N):
     vectors.append([])
@@ -84,22 +82,20 @@ for i in range(num_flakes):
     flake = flakes[i]
     flake.shape("circle")
     flake.shapesize(Rad,Rad)
-    flake.shapesize(outline=Rad)
     flake.color("white")
     flake.speed(20)
+    flake.hideturtle() #hides icon
 
     x=random.randint(-(N-1)*Spacer,(N-1)*Spacer)
     y=random.randint(-(N-1)*Spacer,(N-1)*Spacer)
 
     flake.penup()   
-    flake.goto(x,y)                      #sets position of flake on screen
+    flake.goto(x,y)     #sets position of flake on screen
     flake.pendown()   
 
-    S = 1 / (100)  #speed paramdeter
-    #S = 1  
-    S = 0  
-    S_random = 1 #random speed parameter (variance of random walk draw)
-
+    #Speed parameters
+    S = 1 / (10)  #speed of deterministic component
+    S_random = 1/10 #random speed parameter (variance of random walk draw)
     [flake.dx , flake.dy] = next_step(x,y)
 
 while True:  
@@ -113,23 +109,18 @@ while True:
         [flake.dx , flake.dy] = next_step(x,y)
 
         #checks for boundary wall collision
+        flake.penup()
         if flake.xcor()>(N-1)*Spacer or flake.xcor()<-(N-1)*Spacer:
             flake.dx*=-1
-            flake.penup()
             flake.setx(random.randint(-(N-1)*Spacer,(N-1)*Spacer))
-            flake.pendown()
         if flake.ycor()>(N-1)*Spacer or flake.ycor()<-(N-1)*Spacer:
             flake.dy*=-1
-            flake.penup()
             flake.sety(random.randint(-(N-1)*Spacer,(N-1)*Spacer))
-            flake.pendown()
         if flake.dx==0 and flake.dy==0: #not moving, hop out
-            flake.penup()
             flake.setx(random.randint(-N*Spacer,N*Spacer))
             flake.sety(random.randint(-N*Spacer,N*Spacer))
             [flake.dx , flake.dy] = next_step(x,y)
-            flake.pendown()
-    
+        flake.pendown()
 wn.mainloop()
 
 
